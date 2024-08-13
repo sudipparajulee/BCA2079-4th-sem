4329c0fefd77d98dbb0cf3609275d5ee958c12eb
Sudip Parajuli <sudip.parajulee@gmail.com>
2024-08-13 11:08:05 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.write("Hello World");
+document.write("<h1>Hello World</h1>");
+console.log('BCA 4th Sem');
+// alert('Welcome to Javascript');
+document.getElementById('myheading').innerHTML = 'We are BCA Semester';
